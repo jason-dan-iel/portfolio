389d2b905f12abf1e39c0a985bc88c4f46417856
Jason Daniel <jasondaniel18@gmail.com>
2023-06-22 07:21:57 +0530

    Tried some font animations for the front page

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,7 +1,22 @@
-export default function Navbar(){
-    return(
-        <div className="bg-red-100 text-center font-Cedarville text-[170px]">
-            I am Jason Daniel
-        </div>
-    )
+export default function Navbar() {
+  return (
+    // <div className="bg-red-100 text-center font-Cedarville text-[170px]">
+    //     I am Jason Daniel
+    // </div>
+    <div className="">
+
+      <svg viewBox="0 0 600 300">
+        <symbol id="s-text">
+          <text textAnchor="middle" x="50%" y="50%" dy=".35em">
+            Jason Daniel
+          </text>
+        </symbol>
+        <use className="text" xlinkHref="#s-text"></use>
+        <use className="text" xlinkHref="#s-text"></use>
+        <use className="text" xlinkHref="#s-text"></use>
+        <use className="text" xlinkHref="#s-text"></use>
+        <use className="text" xlinkHref="#s-text"></use>
+      </svg>
+    </div>
+  );
 }
